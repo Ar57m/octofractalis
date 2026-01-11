69effe0e77c8402a9931446ad2e00b29b2302a77
Ar57m <132871733+Ar57m@users.noreply.github.com>
2026-01-11 20:16:23 -0300

    Update custom_quaternion.h

--- custom_quaternion.h
+++ custom_quaternion.h
@@ -27,7 +27,7 @@ private:
     // }
     
     HOST_DEVICE inline DefaultType noNan(DefaultType value) const {
-        return ( my_abs(value) < Max_flt) ? value : 0.0;
+        return ( my_abs(value) < Max_flt) ? value : Max_flt;
     }
 
     HOST_DEVICE inline DefaultType signDefaultType(DefaultType value) const {
@@ -864,4 +864,5 @@ public:
 
 
 #endif
+
 
